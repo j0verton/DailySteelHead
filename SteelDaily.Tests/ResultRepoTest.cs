be39a77e73615a40d5a2b2c5094da7da5a21243a
Joe <overtonje@gmail.com>
2021-01-30 09:33:06 -0600

    setting up for resultsrepo test

---
+++ SteelDaily.Tests/ResultRepoTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+using SteelDaily.Models;
+using SteelDaily.Models.ViewModel;
+
+namespace SteelDaily.Tests
+{
+    public class IntervalFretboardTest
+    {
+        [Fact]
+    publci void 
+        {
+        }
+    }
+}
