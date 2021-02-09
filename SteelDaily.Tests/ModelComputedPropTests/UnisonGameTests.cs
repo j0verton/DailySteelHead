5c0ef8ed57c319a4c93258fde92a581eea69f23e
Joe <overtonje@gmail.com>
2021-02-09 12:44:25 -0600

    starting to write unison game test

---
+++ SteelDaily.Tests/ModelComputedPropTests/UnisonGameTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteelDaily.Tests.ModelComputedPropTests
+{
+    class UnisonGameTests
+    {
+        [Fact]
+        public void Correct_Answer_Should_Create_Correct_Outcome()
+        {
+            var TestTuning = new Tuning()
+            {
+                Id = 1,
+                Name = "E9",
+                Notes = "Gb,Eb,Ab,E,B,Ab,Gb,E,D,B"
+            };
+            var Result1 = new Result()
+            {
+                Id = 1,
+                Key = "C",
+                TuningId = 1,
+                Tuning = TestTuning,
+                Questions = "11,1,8,7,7,3,15,5",
+                Answers = "Two,One,FlatSeven,Two"
+            };
+            //these answers are all off b/c not accouting for 0 index in the second value... 2 is string 3
+            ////ans = D/2
+            //Result1.Questions.Add(new List<int> { 11, 2 });
+            ////ans = C/1
+            //Result1.Questions.Add(new List<int> { 8, 8 });
+            ////ans = B/b7
+            //Result1.Questions.Add(new List<int> { 7, 4 });
+            ////ans = E/3
+            //Result1.Questions.Add(new List<int> { 15, 5 });
+
+            var Game = new InProcessGame()
+            {
+                Result = Result1
+            };
+
+            Assert.True(Game.Outcomes[0]);
+            Assert.True(Game.Outcomes[1]);
+            //Assert.True(Game.Outcomes[2]);
+            //Assert.True(Game.Outcomes[3]);
+
+        }
+    }
+}
