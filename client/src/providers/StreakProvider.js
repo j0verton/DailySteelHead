bd99b43f754db214324f1d5caacdd1bd6dfdc325
Joe <overtonje@gmail.com>
2021-04-02 20:49:14 -0500

    fixed an issue with my firebase project name

--- client/src/providers/StreakProvider.js
+++ client/src/providers/StreakProvider.js
@@ -16,7 +16,10 @@ export function StreakProvider(props) {
                     Authorization: `Bearer ${token}`,
                 },
             })
-        }).then(res => res.json())
+        }).then(res => {
+            console.log(res)
+            return res.json()
+        })
             .then(res => {
                 setStreak(res)
             })
